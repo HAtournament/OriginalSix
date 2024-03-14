8d83936c9bdc6341d27919a43ab0894566a98f5d
ericko <erik.hockeyarena@gmail.com>
2024-03-14 10:18:57 +0100

    Update registrovanych

---
+++ teams.js
@@ -0,0 +1,56 @@
+var islanders = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=58485';
+document.getElementById('islanders').setAttribute('href', islanders);
+
+var pittsburgh = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=9011';
+document.getElementById('pittsburgh').setAttribute('href', pittsburgh);
+
+var anaheim = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=2373';
+document.getElementById('anaheim').setAttribute('href', anaheim);
+
+var rangers = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=59162';
+document.getElementById('rangers').setAttribute('href', rangers);
+
+var buffalo = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=34124';
+document.getElementById('buffalo').setAttribute('href', buffalo);
+
+var nashville = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=31703';
+document.getElementById('nashville').setAttribute('href', nashville);
+
+var carolina = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=12401';
+document.getElementById('carolina').setAttribute('href', carolina);
+
+var dallas = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=28315';
+document.getElementById('dallas').setAttribute('href', dallas);
+
+var sanjose = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=58552';
+document.getElementById('sanjose').setAttribute('href', sanjose);
+
+var florida = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=58824';
+document.getElementById('florida').setAttribute('href', florida);
+
+var jersey = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=28034';
+document.getElementById('jersey').setAttribute('href', jersey);
+
+var vegas = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=58913';
+document.getElementById('vegas').setAttribute('href', vegas);
+
+var chicago = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=59033';
+document.getElementById('chicago').setAttribute('href', chicago);
+
+var toronto = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=59033';
+document.getElementById('toronto').setAttribute('href', toronto);
+
+var winnipeg = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=44968';
+document.getElementById('winnipeg').setAttribute('href', winnipeg);
+
+var stluis = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=41239';
+document.getElementById('stluis').setAttribute('href', stluis);
+
+var lakings = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=2325';
+document.getElementById('lakings').setAttribute('href', lakings);
+
+var tampa = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=32433';
+document.getElementById('tampa').setAttribute('href', tampa);
+
+var washington = 'https://www.hockeyarena.net/sk/index.php?p=public_team_info_basic.php&team_id=2763';
+document.getElementById('washington').setAttribute('href', washington);
