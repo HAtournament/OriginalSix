78228f662eec7a618e0d7b9d9f8f4e4c08133986
ericko <erik.hockeyarena@gmail.com>
2024-03-24 13:25:40 +0100

    Skuska

---
+++ menu.js
@@ -0,0 +1,47 @@
+var navbarHTML = `
+<section class="navigation">
+<div class="nav-container">
+  <div class="brand">
+    <a href="index.html">Original Six</a>
+  </div>
+  <nav>
+    <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+    <ul class="nav-list">
+      <li>
+        <a href="schedule.html">Schedule</a>
+      </li>
+      <li>
+        <a href="table.html">Standings</a>
+      </li>   
+      <li>
+        <a href="statistics.html">Statistics</a>
+      </li>          
+      <!--
+      <li>
+        <a href="#!">Standings</a>
+        <ul class="nav-dropdown">
+          <li>
+            <a href="Standings/season28.html">Season 28</a>
+          </li>
+          <li>
+            <a href="Standings/season29.html">Season 29</a>
+          </li>
+        </ul>
+      </li>
+      -->
+      <li>
+        <a href="playoff.html">Play Off</a>
+      </li>
+      <li>
+        <a href="registration.html">Managers</a>
+      </li>
+      <li>
+        <a href="history.html">History</a>
+      </li>
+    </ul>
+  </nav>
+</div>
+</section>
+`;
+
+document.write(navbarHTML);
