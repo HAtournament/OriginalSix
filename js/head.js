9fbc44858ce8b923352f0d53408ab470defaaa62
ericko <erik.hockeyarena@gmail.com>
2024-03-24 13:54:40 +0100

    Skuska

---
+++ js/head.js
@@ -0,0 +1,14 @@
+var headHTML = `
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv='cache-control' content='no-cache'>
+  <meta http-equiv='expires' content='0'>
+  <meta http-equiv='pragma' content='no-cache'>
+  <title>Schedule</title>
+  <link rel="stylesheet" href="styles.css">
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+</head>
+`;
+
+document.write(headHTML);
